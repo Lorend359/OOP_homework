e5ed4b0a98b79e25a23db852f9ec5885a288c34b
Svyatoslav <arend359@gmail.com>
2024-11-13 23:26:34 +0300

    fixed error

--- src/product_and_category.py
+++ src/product_and_category.py
@@ -5,13 +5,31 @@ class Product:
         """Инициализирует продукт с данными."""
         self.name = name
         self.description = description
-        self._price = price
+        self.__price = price
         self._quantity = quantity
 
     @property
     def quantity(self):
         """Возвращает количество продукта."""
         return self._quantity
+
+    @property
+    def price(self):
+        """Возвращает цену продукта."""
+        return self.__price
+
+    @price.setter
+    def price(self, value: float):
+        """Устанавливает новую цену продукта."""
+        print(f"Попытка установить цену: {value}")
+        if value <= 0:
+            print("Цена не должна быть нулевой или отрицательной")
+            return
+        if value < self.__price:
+            confirm = input("Вы действительно хотите понизить цену? (y/n): ")
+            if confirm.lower() != 'y':
+                return
+        self.__price = value
 
     @classmethod
     def new_product(cls, product_info: dict, existing_products: list = None):
@@ -33,24 +51,6 @@ class Product:
         if existing_products is not None:
             existing_products.append(new_product)
         return new_product
-
-    @property
-    def price(self):
-        """Возвращает цену продукта."""
-        return self._price
-
-    @price.setter
-    def price(self, value: float):
-        """Устанавливает новую цену продукта."""
-        print(f"Попытка установить цену: {value}")
-        if value <= 0:
-            print("Цена не должна быть нулевой или отрицательной")
-            return
-        if value < self._price:
-            confirm = input("Вы действительно хотите понизить цену? (y/n): ")
-            if confirm.lower() != 'y':
-                return
-        self._price = value
 
 class Category:
     """Класс, представляющий категорию продуктов."""
